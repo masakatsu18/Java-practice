3b6624ea9f16e0bfcc6edb692e733f0e076824c5
kogamasakatsu <kogamasakatsu@localhost>
2023-02-22 23:19:20 +0900

    アクセス修飾子

---
+++ src/practice/Car.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class Car {
+	// 燃費（Km/L）
+    private double fuelCost;
+    // 残量（L）
+    private double fuelAmount;
+
+    // コンストラクタを作成
+    public Car() {
+    	fuelCost = 20;
+    	fuelAmount = 53.4;
+    }
+
+    public double getFuelCost() {
+            return fuelCost;
+     }
+    public double getFuelAmount() {
+            return fuelAmount;
+ }
+    }
+    // moveメソッド
+    // ・"xx km 走ります"を出力
+    // ・残量を計算
+
+    // fuelAmountを取得するメソッドを作成
+
