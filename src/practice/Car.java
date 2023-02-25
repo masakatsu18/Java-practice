1fda5deaedb8e1bf055881fdaacd63defa4b3460
mmhhr.since2014@gmail.com <kogamasakatsu@localhost>
2023-02-25 11:45:34 +0900

    例外の基本

--- src/practice/Car.java
+++ src/practice/Car.java
@@ -7,21 +7,17 @@ public class Car {
     private double fuelAmount;
 
     // コンストラクタを作成
-    public Car() {
-    	fuelCost = 20;
-    	fuelAmount = 53.4;
+    public Car(double fuelCost, double fuelAmount) {
+        this.fuelCost = fuelCost;
+        this.fuelAmount = fuelAmount;
     }
 
-    public double getFuelCost() {
-            return fuelCost;
-     }
+    public void move(int km) {
+        System.out.println(km + " km 走ります");
+        this.fuelAmount -= (km / fuelCost);
+    }
+
     public double getFuelAmount() {
-            return fuelAmount;
- }
+        return this.fuelAmount;
     }
-    // moveメソッド
-    // ・"xx km 走ります"を出力
-    // ・残量を計算
-
-    // fuelAmountを取得するメソッドを作成
-
+}
